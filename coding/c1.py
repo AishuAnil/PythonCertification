e412d09cf36cb2c4e72f040fb616ed6b95150070
Aiswarya Anilkumar <aishuu.anil99@gmail.com>
2023-10-30 18:14:47 +0530

    Push uncommited files

---
+++ coding/c1.py
@@ -0,0 +1,5 @@
+#x=1
+text=input("Enter your name")
+while x < 2:   #while True:
+    print(text.capitalize())
+
---
+++ coding/c2.py
@@ -0,0 +1,13 @@
+x=1
+while x < 2:
+    text=(input("What is your name?"))
+    print(text.capitalize())
+
+# countries = []
+#
+# while True:
+#     country = input("Enter the country: ")
+#     countries.append(country)
+#     print(countries)
+
+
